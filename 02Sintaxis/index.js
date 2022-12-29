a53deb1f858ae075043f2d6965b9d69591564a54
Sofía <so.golnner@gmail.com>
2022-12-29 19:40:06 -0300

    segundo ejercicio

---
+++ 02Sintaxis/index.js
@@ -0,0 +1,10 @@
+let nombre = "Sofía Golnner";
+let edad = 21;
+let esDesarrollador = true;
+let nacimiento = new Date(2001, 7, 26);
+const libroFav= {
+  nombre: "Ficciones",
+  autor: "Jorge Luis Borges",
+  publicación: new Date (1944,0,0) 
+      
+};
